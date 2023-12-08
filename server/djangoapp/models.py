f4f62e696e655853e9e282b8f76148d30b0a0091
Micah-Black <117412390+Micah-Black@users.noreply.github.com>
2023-12-07 18:32:33 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,10 +5,19 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
+class CarMake:
+
+    def __init__(self, name, description):
+        # - Name
+        self.name = name
+        # - Description
+        self.description = description
 # - __str__ method to print a car make object
+    def __str__(self):
+        return "Make name: " + self.name
+
+# - Any other fields you would like to include in car make model
+
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -22,6 +31,29 @@ from django.utils.timezone import now
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
